551510aedb7c3d7dc82ee8eb75c7aeaebd32d106
NotTayyy <robertantoine99@gmail.com>
2022-05-14 16:08:43 -0400

    Dont

--- index-java.js
+++ index-java.js
@@ -13,12 +13,16 @@ const apiUrl = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=';
 SearchBoxMainNav.addEventListener('submit', async(event) => {
   event.preventDefault();
   const searchText = document.getElementById('SearchTerm').value;
-
   cardSearchApi = apiUrl + searchText;
   console.log(cardSearchApi);
-
+try {
   const response = await fetch(cardSearchApi);
   const data = await response.json();
   console.log(data.data[0]);
-});
+  alert('Name: ' + data.data[0].name + '\n\n' + 'Type: ' + data.data[0].type + '\n\n' + 'Description: ' + data.data[0].desc);
 
+} catch { alert("ERROR Please submit a Yu-Gi-Oh Card Name!"); 
+//----in the future When the user types into the Search box, It will auto update a Dropdown menu populated with the first 5 options from the array 
+//----that shows up so if you type in b a ccunction will call this code and pull whatever arrray it returns then taking the first 45 items from 
+//----the array it will populate the Dropdown withh those 5, Making them Clickable and Making it ewasier to find the correct card
+}});
