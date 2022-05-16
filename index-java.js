e94cd1bd8b15f6e0583ebbbabb0b3a72421d0b64
NotTayyy <robertantoine99@gmail.com>
2022-05-16 19:45:47 -0400

    FINALLY FINISHED THE CASRD SEARCH FUNCTION AND LOOPPPPP

--- index-java.js
+++ index-java.js
@@ -6,23 +6,60 @@ document.getElementById("headeroverlay").style.backgroundImage = headerBG[random
 //-- End of Randomization
 
 
+//--------------------I Was using this for API Calls, Might use again for api Calls after learning Back End
+//  const FapiUrl = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=';
+//  SearchBoxMainNav.addEventListener('submit', async(event) => {
+//    event.preventDefault();
+//    const searchText = document.getElementById('SearchTerm').value;
+//    cardSearchApi = FapiUrl + searchText;
+//    console.log(cardSearchApi);
+//     try {
+//    const response = await fetch(cardSearchApi);
+//    const data = await response.json();
+//    console.log(data[0]);
+//    alert('Name: ' + data.data[0].name + '\n\n' + 'Type: ' + data.data[0].type + '\n\n' + 'Description: ' + data.data[0].desc);
 
-//Main Search Loop Uses a Function to Take the Param of the Search box in order to search the Api For A Name.
-const apiUrl = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=';
+//  } catch { alert("ERROR Please submit a Yu-Gi-Oh Card Name!"); 
+//  }});
+//------------------------------------------------
 
+//-----Main Search Loop Uses a Function to Take the Param of the Search box in order to search the Api For A Name.
+function cardSearch(input) {
+  var result = [];
+  for (var i=0; i < cardAPIListings.data.length ; i++ ) {
+    if (cardAPIListings.data[i]["name"].toUpperCase().includes(input.toUpperCase())) {
+      if (result.length < 5) {
+        result.push(cardAPIListings.data[i]);
+      }
+    }
+  }
+  console.log(result);
+
+  return result;
+}
+//-- Updates the Text Whenever an Input is made to the Text Field
+var inputlog = document.getElementById("placeholder");
+
+SearchBoxMainNav.addEventListener('input', async(event) =>{
+  var input = document.getElementById('SearchTerm').value;
+  inputlog.innerHTML = "";
+  if (input != "") {
+    var result = cardSearch(input);
+    for (var i = 0; i < 4; i++) {
+      inputlog.innerHTML += '['+result[i].name +'] ' ;
+    }
+  } 
+});
+
+//Alerts the page whenever you Submit the Form search Field
 SearchBoxMainNav.addEventListener('submit', async(event) => {
   event.preventDefault();
-  const searchText = document.getElementById('SearchTerm').value;
-  cardSearchApi = apiUrl + searchText;
-  console.log(cardSearchApi);
-try {
-  const response = await fetch(cardSearchApi);
-  const data = await response.json();
-  console.log(data.data[0]);
-  alert('Name: ' + data.data[0].name + '\n\n' + 'Type: ' + data.data[0].type + '\n\n' + 'Description: ' + data.data[0].desc);
-
-} catch { alert("ERROR Please submit a Yu-Gi-Oh Card Name!"); 
-//----in the future When the user types into the Search box, It will auto update a Dropdown menu populated with the first 5 options from the array 
-//----that shows up so if you type in "b" a function will call this code and pull whatever array it returns then taking the first 5 items from 
-//----the array it will populate the Dropdown with those 5, Making them Clickable and Making it easier to find the correct card
-}});
+  var searchText = document.getElementById('SearchTerm').value;
+  var result = cardSearch(searchText);
+  
+  if (result.length == 0) {
+    alert("ERROR Please submit a Yu-Gi-Oh Card Name!" + result.length);
+  } else {
+    alert('Name: ' + result[0].name + '\n\n' + 'Type: ' + result[0].type + '\n\n' + 'Description: ' + result[0].desc + '\n\n' + 'Length: ' + result.length);
+  }
+});
