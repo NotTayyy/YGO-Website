dbb0ca7ef1a576d22c3b9fe15c91a0bf75e4fc5b
NotTayyy <robertantoine99@gmail.com>
2022-08-03 08:36:27 -0400

    Clean Up

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const headerBG = ["url(Images/Header/HBG1.jpg)", "url(Images/Header/HBG2.jpg)", 
   "url(Images/Header/HBG5.jpg)", "url(Images/Header/HBG6.jpg)", "url(Images/Header/HBG7.jpg)", "url(Images/Header/HBG8.jpg)", 
   "url(Images/Header/HBG9.jpg)", "url(Images/Header/HBG10.jpg)", "url(Images/Header/HBG11.jpg)", "url(Images/Header/HBG12.jpg)"];
 
-function getAPI(){
+function getAPI() {
   fetch('https://ygo-site-backend.herokuapp.com/cardinfo.json', {
     method: 'GET',
     headers: {
       accept: 'application/json',
-    },})
+    },
+  }) 
     .then(function(response) {
       console.log(response)
       return response.json();
